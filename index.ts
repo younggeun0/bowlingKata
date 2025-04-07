f5c93eb34d4c0b74ba58c49067bc4e580ddd5863
Young Oh <dureng5@gmail.com>
2025-04-07 14:08:34 +0900

    TheFourthTest: update score to pass one strike test

--- index.ts
+++ index.ts
@@ -15,12 +15,17 @@ class Game {
         let frameIndex = 0;
 
         for (let frame = 0; frame < 10; frame++) {
-            if (this.isSpare(frameIndex)) {
+            if (this.rolls[frameIndex] === 10) {
+                // strike
+                totalScore += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+                frameIndex += 1;
+            } else if (this.isSpare(frameIndex)) {
                 totalScore += 10 + this.rolls[frameIndex + 2];
+                frameIndex += 2;
             } else {
                 totalScore += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+                frameIndex += 2;
             }
-            frameIndex += 2;
         }
 
         return totalScore;
